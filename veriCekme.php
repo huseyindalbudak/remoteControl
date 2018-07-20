6810a46fce96006a00fea47921e31989540859dd
huseyin dalbudak <93huseyin93@gmail.com>
2018-07-20 23:20:44 +0300

    Update veriCekme.php

--- veriCekme.php
+++ veriCekme.php
@@ -7,6 +7,7 @@
     <title>veriCekme</title>
 </head>
 <body>
+    
 <?php
 
 // it can take any type of data according to html type in a website
@@ -14,20 +15,20 @@
 
 $veri = file_get_contents("http://bigpara.hurriyet.com.tr/viop-varant/canli-varant-verileri");
 
-//echo strip_tags($veri);
+//preg_match_all() is special php function
 preg_match_all('@ <li class="cell048 node-c" id="h_td_fiyat_id_UZIGGV">(.*?)</li>@si',$veri,$baslik);
-//preg_match_all('@<span id="priceblock_ourprice" class="a-size-medium a-color-price">(.*?)</span>@si',$veri,$ucret);
 
-//print_r($baslik);
+// veri is array 
 $veri= $baslik[0][0];
 echo $veri  ;
-echo "\n"; // bir sonraki satir icin
-echo "<br>" ;
-echo var_dump($baslik[0][0]);
-//echo $ucret[0][0];
+echo "<br>" ;  //for next line in html
+echo var_dump($baslik[0][0]);  // output data type via php function
 ?>
+    
+    
 <p id="demom">test</p>
 
+    
 <script>
 var text = document.getElementById("h_td_fiyat_id_UZIGGV").innerHTML;
 alert(text);
